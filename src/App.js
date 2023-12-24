2ee39292a59e10539da53f0ab9ebacd8709bc4a3
ethanoutangoun <ethanoutangoun@gmail.com>
2023-12-24 21:32:51 +0700

    add footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Intro from "./Intro";
 import Featured from "./Featured";
+import Footer from "./Footer";
 
 const App = () => {
   return (
     <div className="app">
       <Intro />
       <Featured />
+      <Footer />
     </div>
   );
 };
---
+++ src/Footer.js
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <div className="footer">© 2023 Ethan Outangoun</div>;
+};
+
+export default Footer;
