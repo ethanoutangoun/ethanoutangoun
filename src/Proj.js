18e0ad999dd35e13df895c1834035e72bf448403
ethanoutangoun <ethanoutangoun@gmail.com>
2023-12-24 20:12:12 +0700

    add github section

--- src/Proj.js
+++ src/Proj.js
@@ -1,22 +1,24 @@
-const Proj = ({link, title, tools, description, imageUrl}) => {
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 
-    const imageAlt = title + "-img";
+const Proj = ({ link, title, tools, description, imageUrl }) => {
+  const imageAlt = title + "-img";
 
-    return ( 
+  return (
     <div className="proj">
-    <img
-      src={imageUrl}
-      alt={imageAlt}
-      onClick={() => window.open(link)}
-    />
-    <h2 onClick={() => window.open(link)}>
-      {title}
-    </h2>
-    <h5>{tools}</h5>
-    <p>
-      {description}
-    </p>
-  </div> );
-}
- 
+      <img src={imageUrl} alt={imageAlt} onClick={() => window.open(link)} />
+
+      <h2 onClick={() => window.open(link)}>
+        {title}
+        <ExitToAppIcon />
+      </h2>
+      <h5>{tools}</h5>
+      <p>{description}</p>
+
+      <div className="github-section">
+        <img src="github.png" alt="github" onClick={() => window.open(link)} />
+      </div>
+    </div>
+  );
+};
+
 export default Proj;
