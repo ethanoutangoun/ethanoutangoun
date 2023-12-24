2e0ea1f03f37c6da00ee71ced5402257b9c4eb2e
ethanoutangoun <ethanoutangoun@gmail.com>
2023-12-24 21:24:22 +0700

    add github and linkedin button to site

--- src/Intro.js
+++ src/Intro.js
@@ -1,4 +1,5 @@
 import FileDownloadIcon from "@mui/icons-material/FileDownload";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const Intro = () => {
   function scrollToTop() {
@@ -25,12 +26,25 @@ const Intro = () => {
               and full-stack/frontend development.
             </p>
 
+            <div className="intro-buttons">
             <a className="resume" href="resume.pdf" target="_blank">
               <div className="resume-inner">
                 <p>Resume</p>
                 <FileDownloadIcon />
               </div>
             </a>
+
+        
+              <div className="i-button" onClick={()=>window.open('https://www.linkedin.com/in/ethanoutangoun/')}>
+                <img src="linkedin.png" alt="linkedin-button"/>
+              </div>
+
+              <div className="i-button" onClick={()=>window.open('https://github.com/ethanoutangoun')}>
+                <img src="github-icon.png" alt="linkedin-button"/>
+              </div>
+          
+            </div>
+            
           </div>
 
           <img
